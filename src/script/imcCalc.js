c8d4614845279a1d217decf482c079589736826d
Fernando Stadler <51368071+fernandoStadler@users.noreply.github.com>
2020-07-22 23:27:21 -0300

    validações e mask

--- src/script/imcCalc.js
+++ src/script/imcCalc.js
@@ -1,6 +1,17 @@
-function imc ()
 
-{
+
+//Mask 
+$("#altura").mask("0.00");
+$("#peso").mask("00.00");
+
+
+let peso = document.getElementById("peso").value;
+let altura = document.getElementById("altura").value;
+
+let newText = document.getElementById("result");
+let btn = document.getElementById("btnCalc");
+
+function imc() {
 
     let peso = document.getElementById("peso").value;
     let altura = document.getElementById("altura").value;
@@ -8,22 +19,26 @@ function imc ()
     let sqrAltura;
     let newText = document.getElementById("result");
 
-    sqrAltura = altura*altura;
+    sqrAltura = altura * altura;
+    result = peso / sqrAltura;
 
-    result=peso /sqrAltura;
-
-    newText.textContent = parseFloat(result.toFixed(2));
-
+    newText.textContent = parseFloat(result.toFixed(2))
 }
 
+function validate() {
+    if (this.altura.value == '' | this.peso.value == '') {
+        btn.disabled = true
+    }
+    else {
+        const btn = document.getElementById("btnCalc");
+        btn.disabled = false
+    }
+}
 
-function clean(){
-
-    let peso = document.getElementById("peso").value="";
-    let altura = document.getElementById("altura").value="";
-    let newText = document.getElementById("result").textContent="O seu IMC é! ";
-
-    
-
+function clean() {
+    this.peso.value = "";
+    this.altura.value = "";
+    newText.textContent = "O seu IMC é! ";
+    validate();
 }
 
