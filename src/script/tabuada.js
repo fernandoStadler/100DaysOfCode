67bc19d0ec46333920b10daeb975058dc6541f23
Fernando Stadler <51368071+fernandoStadler@users.noreply.github.com>
2020-07-25 19:58:10 -0300

    atualização da função clear

--- src/script/tabuada.js
+++ src/script/tabuada.js
@@ -4,8 +4,7 @@
 $("#multiplicando").mask("0000");
 $("#multiplicador").mask("0000");
 
-function mult() 
-{
+function mult() {
 
     let i;
     let multiplier = parseFloat(document.getElementById("multiplicador").value);
@@ -13,8 +12,7 @@ function mult()
     let result;
     let modal = document.getElementById("multResult");
 
-    for (i = 0; i < multiplier + 1; i++) 
-    {
+    for (i = 0; i < multiplier + 1; i++) {
         result = multiplying * i;
         modal.innerHTML += multiplying + " x " + (i) + " = " + result + "<br/>";
     }
@@ -24,35 +22,54 @@ function mult()
 
 
 
-function validate(){
+function validate() {
 
+    $("#exampleModalLong").on("hide.bs.modal", function (e) {
+        let modal = document.getElementById("multResult");
+        multiplier = document.getElementById("multiplicador").value = "";
+        multiplying = document.getElementById("multiplicando").value = "";
+        modal.innerHTML = "";
+
+
+        validate();
+    })
     let btn_mult = document.getElementById("btn_mult");
     let multiplicador = document.getElementById("multiplicador");
     let multiplicando = document.getElementById("multiplicando");
-   
-   if(multiplicador.value  == "" | multiplicando.value==""){
-    
-    btn_mult.disabled = true;    
+    let modalBoot = document.getElementById("exampleModalLong");
 
-   }
+    if (multiplicador.value == "" | multiplicando.value == "") {
 
-   else {
+        btn_mult.disabled = true;
+        modalBoot = document.getElementById("exampleModalLong").className = "modal fade";
 
-    
-    btn_mult.disabled = false;
-   }
+
+
+
+    }
+
+    else {
+
+
+        modalBoot = document.getElementById("exampleModalLong").className = "modal fade show";
+        btn_mult.disabled = false;
+
+
+
+    }
 }
 
-function clean() 
-{
-  
+function clean() {
+
+
 
     let modal = document.getElementById("multResult");
     multiplier = document.getElementById("multiplicador").value = "";
     multiplying = document.getElementById("multiplicando").value = "";
     modal.innerHTML = "";
+
+
     validate();
+
 }
-
-
 
